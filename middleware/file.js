c7e4b5e7cadd40c7fa0136167dcf96ba7ce2b7b3
Каверза Артем <akaverza@phoenixit.ru>
2023-02-23 20:07:08 +0300

    task 2: upload/download book

---
+++ middleware/file.js
@@ -0,0 +1,24 @@
+const multer = require('multer');
+const { UPLOADED_FILE_PATH } = require('../constants');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, UPLOADED_FILE_PATH);
+    },
+    filename: (req, file, cb) => {
+        const [exp] = /[^.]+$/.exec(file.originalname);
+        cb(null, Date.now() + '.' + exp);
+    }
+});
+
+module.exports = (req, res, next) => {
+    return multer({ storage }).single('fileBook')(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            if (err.code === 'LIMIT_UNEXPECTED_FILE') {
+                res.status(400);
+                res.json(err);
+            }
+        }
+        next();
+    } );
+}
--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 const db = require('../database/db');
 const Book = require('../models/Book');
+const { UPLOADED_FILE_PATH } = require('../constants');
+const fileMulter = require('../middleware/file');
+
+const router = express.Router();
 
 router.get('/', (req, res) => {
     const { books } = db;
@@ -22,7 +27,7 @@ router.get('/:id', (req, res) => {
     res.json('[Book] - page not found');
 });
 
-router.post('/', (req, res) => {
+router.post('/', fileMulter, (req, res) => {
     const { books } = db;
     const { 
         title = null,
@@ -31,21 +36,48 @@ router.post('/', (req, res) => {
         favorite = null,
         fileCover = null,
         fileName = null,
-     } = req.body;
+    } = req.body;
 
+    const fileBook = req.file ? req.file.filename : null;
     const newBook = new Book(
         title,
         description,
         authors,
         favorite,
         fileCover,
-        fileName
+        fileName,
+        fileBook
     );
 
     books.push(newBook);
 
     res.status(201);
     res.json(newBook);
+});
+
+router.get('/:id/download', (req, res) => {
+    const { id } = req.params;
+    const { books } = db;
+    const idx = books.findIndex(item => item.id === id)
+
+    if (idx >= 0) {
+        const fileName = books[idx].fileBook;
+        if (!fileName) {
+            res.status(404);
+            res.json('[Book] - file not found');
+        }
+
+        const filePath = path.join(UPLOADED_FILE_PATH, books[idx].fileBook);
+        const fileExists = fs.existsSync(filePath);
+
+        if (fileExists) return res.download(filePath);
+
+        res.status(404);
+        res.json('[Book] - file not found');
+    }
+
+    res.status(404);
+    res.json('[Book] - page not found');
 });
 
 router.put('/:id', (req, res) => {
